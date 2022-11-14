f9d944db89405655703462053b23da16af5801be
Ben Baeten <bmbaeteten@gmail.com>
2022-11-14 09:52:33 -0600

    added more logging, error handling

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 #include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
 
 #define FILESYSTEM SPIFFS
-#define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
-#define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
-#define PANEL_CHAIN 2      // Total number of panels chained one to another
+#define PANEL_RES_X 64 // Number of pixels wide of each INDIVIDUAL panel module.
+#define PANEL_RES_Y 32 // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 2  // Total number of panels chained one to another
 
-//Ben Comment
+// Ben Comment
 
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
@@ -27,43 +27,48 @@ int x_offset, y_offset;
 String listFiles(bool ishtml = false);
 // Make size of files human readable
 // source: https://github.com/CelliesProjects/minimalUploadAuthESP32
-String humanReadableSize(const size_t bytes) {
-  if (bytes < 1024) return String(bytes) + " B";
-  else if (bytes < (1024 * 1024)) return String(bytes / 1024.0) + " KB";
-  else if (bytes < (1024 * 1024 * 1024)) return String(bytes / 1024.0 / 1024.0) + " MB";
-  else return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
+String humanReadableSize(const size_t bytes)
+{
+  if (bytes < 1024)
+    return String(bytes) + " B";
+  else if (bytes < (1024 * 1024))
+    return String(bytes / 1024.0) + " KB";
+  else if (bytes < (1024 * 1024 * 1024))
+    return String(bytes / 1024.0 / 1024.0) + " MB";
+  else
+    return String(bytes / 1024.0 / 1024.0 / 1024.0) + " GB";
 }
 
-
-
-void rebootESP(String message) {
-  Serial.print("Rebooting ESP32: "); Serial.println(message);
+void rebootESP(String message)
+{
+  Serial.print("Rebooting ESP32: ");
+  Serial.println(message);
   ESP.restart();
 }
 
 // Draw a line of image directly on the LED Matrix
 void GIFDraw(GIFDRAW *pDraw)
 {
+  try
+  {
     uint8_t *s;
     uint16_t *d, *usPalette, usTemp[320];
     int x, y, iWidth;
 
-  iWidth = pDraw->iWidth;
-  if (iWidth > MATRIX_WIDTH)
+    iWidth = pDraw->iWidth;
+    if (iWidth > MATRIX_WIDTH)
       iWidth = MATRIX_WIDTH;
-
-    Serial.println("Starting Draw");
 
     usPalette = pDraw->pPalette;
     y = pDraw->iY + pDraw->y; // current line
-    
+
     s = pDraw->pPixels;
     if (pDraw->ucDisposalMethod == 2) // restore to background color
     {
-      for (x=0; x<iWidth; x++)
+      for (x = 0; x < iWidth; x++)
       {
         if (s[x] == pDraw->ucTransparent)
-           s[x] = pDraw->ucBackground;
+          s[x] = pDraw->ucBackground;
       }
       pDraw->ucHasTransparency = 0;
     }
@@ -75,9 +80,9 @@ void GIFDraw(GIFDRAW *pDraw)
       pEnd = s + pDraw->iWidth;
       x = 0;
       iCount = 0; // count non-transparent pixels
-      while(x < pDraw->iWidth)
+      while (x < pDraw->iWidth)
       {
-        c = ucTransparent-1;
+        c = ucTransparent - 1;
         d = usTemp;
         while (c != ucTransparent && s < pEnd)
         {
@@ -88,13 +93,14 @@ void GIFDraw(GIFDRAW *pDraw)
           }
           else // opaque
           {
-             *d++ = usPalette[c];
-             iCount++;
+            *d++ = usPalette[c];
+            iCount++;
           }
-        } // while looking for opaque pixels
+        }           // while looking for opaque pixels
         if (iCount) // any opaque pixels?
         {
-          for(int xOffset = 0; xOffset < iCount; xOffset++ ){
+          for (int xOffset = 0; xOffset < iCount; xOffset++)
+          {
             dma_display->drawPixel(x + xOffset, y, usTemp[xOffset]); // 565 Color Format
           }
           x += iCount;
@@ -106,9 +112,9 @@ void GIFDraw(GIFDRAW *pDraw)
         {
           c = *s++;
           if (c == ucTransparent)
-             iCount++;
+            iCount++;
           else
-             s--; 
+            s--;
         }
         if (iCount)
         {
@@ -121,76 +127,109 @@ void GIFDraw(GIFDRAW *pDraw)
     {
       s = pDraw->pPixels;
       // Translate the 8-bit pixels through the RGB565 palette (already byte reversed)
-      for (x=0; x<pDraw->iWidth; x++)
+      for (x = 0; x < pDraw->iWidth; x++)
       {
         dma_display->drawPixel(x, y, usPalette[*s++]); // color 565
       }
     }
-    Serial.print("Ending Drawing");
+  }
+  catch (const std::exception &e)
+  {
+    Serail.println(e.what());
+  }
+
 } /* GIFDraw() */
 
-String processor(const String& var) {
-  if (var == "FILELIST") {
+String processor(const String &var)
+{
+  if (var == "FILELIST")
+  {
     return listFiles(true);
   }
-  if (var == "FREESPIFFS") {
+  if (var == "FREESPIFFS")
+  {
     return humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes()));
   }
 
-  if (var == "USEDSPIFFS") {
+  if (var == "USEDSPIFFS")
+  {
     return humanReadableSize(SPIFFS.usedBytes());
   }
 
-  if (var == "TOTALSPIFFS") {
+  if (var == "TOTALSPIFFS")
+  {
     return humanReadableSize(SPIFFS.totalBytes());
   }
 
   return String();
 }
 
-void * GIFOpenFile(const char *fname, int32_t *pSize)
+void *GIFOpenFile(const char *fname, int32_t *pSize)
 {
-  Serial.print("Playing gif: ");
-  Serial.println(fname);
-  f = FILESYSTEM.open(fname);
-  if (f)
+  try
   {
-    *pSize = f.size();
-    return (void *)&f;
+    Serial.print("Playing gif: ");
+    Serial.println(fname);
+    f = FILESYSTEM.open(fname);
+    if (f)
+    {
+      *pSize = f.size();
+      return (void *)&f;
+    }
+    else
+    {
+      Serial.println("File open failed");
+    }
+  }
+  catch (const std::exception &e)
+  {
+    Serial.println(e.what());
   }
   return NULL;
+
 } /* GIFOpenFile() */
 
 void GIFCloseFile(void *pHandle)
 {
   File *f = static_cast<File *>(pHandle);
   if (f != NULL)
-     f->close();
+    f->close();
 } /* GIFCloseFile() */
 
 int32_t GIFReadFile(GIFFILE *pFile, uint8_t *pBuf, int32_t iLen)
 {
+  try
+  {
+    Serial.println("Read start");
     int32_t iBytesRead;
     iBytesRead = iLen;
     File *f = static_cast<File *>(pFile->fHandle);
     // Note: If you read a file all the way to the last byte, seek() stops working
     if ((pFile->iSize - pFile->iPos) < iLen)
-       iBytesRead = pFile->iSize - pFile->iPos - 1; // <-- ugly work-around
+      iBytesRead = pFile->iSize - pFile->iPos - 1; // <-- ugly work-around
     if (iBytesRead <= 0)
-       return 0;
+      return 0;
     iBytesRead = (int32_t)f->read(pBuf, iBytesRead);
     pFile->iPos = f->position();
+    Serial.println("Read end");
+
     return iBytesRead;
+  }
+  catch (const std::exception &e)
+  {
+    Serial.println(e.what());
+    return 0;
+  }
 } /* GIFReadFile() */
 
 int32_t GIFSeekFile(GIFFILE *pFile, int32_t iPosition)
-{ 
+{
   int i = micros();
   File *f = static_cast<File *>(pFile->fHandle);
   f->seek(iPosition);
   pFile->iPos = (int32_t)f->position();
   i = micros() - i;
-//  Serial.printf("Seek time = %d us\n", i);
+  //  Serial.printf("Seek time = %d us\n", i);
   return pFile->iPos;
 } /* GIFSeekFile() */
 
@@ -198,25 +237,28 @@ unsigned long start_tick = 0;
 
 void ShowGIF(char *name)
 {
+  Serial.println("Show GIF start");
   start_tick = millis();
-   
+
   if (gif.open(name, GIFOpenFile, GIFCloseFile, GIFReadFile, GIFSeekFile, GIFDraw))
   {
-    x_offset = (MATRIX_WIDTH - gif.getCanvasWidth())/2;
-    if (x_offset < 0) x_offset = 0;
-    y_offset = (MATRIX_HEIGHT - gif.getCanvasHeight())/2;
-    if (y_offset < 0) y_offset = 0;
+    x_offset = (MATRIX_WIDTH - gif.getCanvasWidth()) / 2;
+    if (x_offset < 0)
+      x_offset = 0;
+    y_offset = (MATRIX_HEIGHT - gif.getCanvasHeight()) / 2;
+    if (y_offset < 0)
+      y_offset = 0;
     Serial.printf("Successfully opened GIF; Canvas size = %d x %d\n", gif.getCanvasWidth(), gif.getCanvasHeight());
     Serial.flush();
     while (gif.playFrame(true, NULL))
-    {      
-      if ( (millis() - start_tick) > 8000) { // we'll get bored after about 8 seconds of the same looping gif
+    {
+      if ((millis() - start_tick) > 8000)
+      { // we'll get bored after about 8 seconds of the same looping gif
         break;
       }
     }
     gif.close();
   }
-
 }
 
 const String default_ssid = "Mink's Den";
@@ -244,33 +286,37 @@ const char index_html[] PROGMEM = R"rawliteral(
 )rawliteral";
 
 // configuration structure
-struct Config {
-  String ssid;               // wifi ssid
-  String wifipassword;       // wifi password
-  int webserverporthttp;     // http port number for web admin
+struct Config
+{
+  String ssid;           // wifi ssid
+  String wifipassword;   // wifi password
+  int webserverporthttp; // http port number for web admin
 };
 
 // handles uploads
-void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final) {
+void handleUpload(AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final)
+{
   String logmessage = "Client:" + request->client()->remoteIP().toString() + " " + request->url();
   Serial.println(logmessage);
 
-  if (!index) {
+  if (!index)
+  {
     logmessage = "Upload Start: " + String(filename);
     // open the file on first call and store the file handle in the request object
-    request->_tempFile = SPIFFS.open("/" + filename, "w");
+    request->_tempFile = SPIFFS.open("/gifs" + filename, "w");
     Serial.println(logmessage);
-    
   }
 
-  if (len) {
+  if (len)
+  {
     // stream the incoming chunk to the opened file
     request->_tempFile.write(data, len);
     logmessage = "Writing file: " + String(filename) + " index=" + String(index) + " len=" + String(len);
     Serial.println(logmessage);
   }
 
-  if (final) {
+  if (final)
+  {
     logmessage = "Upload Complete: " + String(filename) + ",size: " + String(index + len);
     // close the file handle as the upload is now done
     request->_tempFile.close();
@@ -280,29 +326,32 @@ void handleUpload(AsyncWebServerRequest *request, String filename, size_t index,
 }
 
 // variables
-Config config;                        // configuration
-AsyncWebServer *server;               // initialise webserver
+Config config;          // configuration
+AsyncWebServer *server; // initialise webserver
 
-void configureWebServer() {
-  server->on("/", HTTP_GET, [](AsyncWebServerRequest * request) {
+void configureWebServer()
+{
+  server->on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+             {
     String logmessage = "Client:" + request->client()->remoteIP().toString() + + " " + request->url();
     Serial.println(logmessage);
-    request->send_P(200, "text/html", index_html, processor);
-  });
+    request->send_P(200, "text/html", index_html, processor); });
 
   // run handleUpload function when any file is uploaded
-  server->on("/upload", HTTP_POST, [](AsyncWebServerRequest *request) {
-        request->send(200);
-      }, handleUpload);
+  server->on(
+      "/upload", HTTP_POST, [](AsyncWebServerRequest *request)
+      { request->send(200); },
+      handleUpload);
 }
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
 
   HUB75_I2S_CFG mxconfig(
-    PANEL_RES_X,   // module width
-    PANEL_RES_Y,   // module height
-    PANEL_CHAIN    // Chain length
+      PANEL_RES_X, // module width
+      PANEL_RES_Y, // module height
+      PANEL_CHAIN  // Chain length
   );
 
   mxconfig.gpio.e = 18;
@@ -312,11 +361,11 @@ void setup() {
   // Display Setup
   dma_display = new MatrixPanel_I2S_DMA(mxconfig);
   dma_display->begin();
-  dma_display->setBrightness8(90); //0-255
+  dma_display->setBrightness8(90); // 0-255
   dma_display->clearScreen();
   dma_display->fillScreen(myWHITE);
-    dma_display->begin();
-  
+  dma_display->begin();
+
   /* all other pixel drawing functions can only be called after .begin() */
   dma_display->fillScreen(dma_display->color565(0, 0, 0));
   gif.begin(LITTLE_ENDIAN_PIXELS);
@@ -324,16 +373,20 @@ void setup() {
   Serial.println("Booting ...");
 
   Serial.println("Mounting SPIFFS ...");
-  if (!SPIFFS.begin(true)) {
+  if (!SPIFFS.begin(true))
+  {
     // if you have not used SPIFFS before on a ESP32, it will show this error.
     // after a reboot SPIFFS will be configured and will happily work.
     Serial.println("ERROR: Cannot mount SPIFFS, Rebooting");
     rebootESP("ERROR: Cannot mount SPIFFS, Rebooting");
   }
 
-  Serial.print("SPIFFS Free: "); Serial.println(humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes())));
-  Serial.print("SPIFFS Used: "); Serial.println(humanReadableSize(SPIFFS.usedBytes()));
-  Serial.print("SPIFFS Total: "); Serial.println(humanReadableSize(SPIFFS.totalBytes()));
+  Serial.print("SPIFFS Free: ");
+  Serial.println(humanReadableSize((SPIFFS.totalBytes() - SPIFFS.usedBytes())));
+  Serial.print("SPIFFS Used: ");
+  Serial.println(humanReadableSize(SPIFFS.usedBytes()));
+  Serial.print("SPIFFS Total: ");
+  Serial.println(humanReadableSize(SPIFFS.totalBytes()));
 
   Serial.println(listFiles());
 
@@ -345,23 +398,35 @@ void setup() {
 
   Serial.print("\nConnecting to Wifi: ");
   WiFi.begin(config.ssid.c_str(), config.wifipassword.c_str());
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
 
   Serial.println("\n\nNetwork Configuration:");
   Serial.println("----------------------");
-  Serial.print("         SSID: "); Serial.println(WiFi.SSID());
-  Serial.print("  Wifi Status: "); Serial.println(WiFi.status());
-  Serial.print("Wifi Strength: "); Serial.print(WiFi.RSSI()); Serial.println(" dBm");
-  Serial.print("          MAC: "); Serial.println(WiFi.macAddress());
-  Serial.print("           IP: "); Serial.println(WiFi.localIP());
-  Serial.print("       Subnet: "); Serial.println(WiFi.subnetMask());
-  Serial.print("      Gateway: "); Serial.println(WiFi.gatewayIP());
-  Serial.print("        DNS 1: "); Serial.println(WiFi.dnsIP(0));
-  Serial.print("        DNS 2: "); Serial.println(WiFi.dnsIP(1));
-  Serial.print("        DNS 3: "); Serial.println(WiFi.dnsIP(2));
+  Serial.print("         SSID: ");
+  Serial.println(WiFi.SSID());
+  Serial.print("  Wifi Status: ");
+  Serial.println(WiFi.status());
+  Serial.print("Wifi Strength: ");
+  Serial.print(WiFi.RSSI());
+  Serial.println(" dBm");
+  Serial.print("          MAC: ");
+  Serial.println(WiFi.macAddress());
+  Serial.print("           IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("       Subnet: ");
+  Serial.println(WiFi.subnetMask());
+  Serial.print("      Gateway: ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("        DNS 1: ");
+  Serial.println(WiFi.dnsIP(0));
+  Serial.print("        DNS 2: ");
+  Serial.println(WiFi.dnsIP(1));
+  Serial.print("        DNS 3: ");
+  Serial.println(WiFi.dnsIP(2));
 
   // configure web server
   Serial.println("\nConfiguring Webserver ...");
@@ -373,28 +438,35 @@ void setup() {
   server->begin();
 }
 
-String gifDir = "/"; // play all GIFs in this directory on the SD card
-char filePath[256] = { 0 };
+String gifDir = "/gifs"; // play all GIFs in this directory on the SD card
+char filePath[256] = {0};
 File root, gifFile;
 
 // list all of the files, if ishtml=true, return html rather than simple text
-String listFiles(bool ishtml) {
+String listFiles(bool ishtml)
+{
   String returnText = "";
   Serial.println("Listing files stored on SPIFFS");
   File root = SPIFFS.open("/");
   File foundfile = root.openNextFile();
-  if (ishtml) {
+  if (ishtml)
+  {
     returnText += "<table><tr><th align='left'>Name</th><th align='left'>Size</th></tr>";
   }
-  while (foundfile) {
-    if (ishtml) {
+  while (foundfile)
+  {
+    if (ishtml)
+    {
       returnText += "<tr align='left'><td>" + String(foundfile.name()) + "</td><td>" + humanReadableSize(foundfile.size()) + "</td></tr>";
-    } else {
+    }
+    else
+    {
       returnText += "File: " + String(foundfile.name()) + "\n";
     }
     foundfile = root.openNextFile();
   }
-  if (ishtml) {
+  if (ishtml)
+  {
     returnText += "</table>";
   }
   root.close();
@@ -402,35 +474,34 @@ String listFiles(bool ishtml) {
   return returnText;
 }
 
-void loop() 
+void loop()
 {
-   while (1) // run forever
-   {
-      
-      root = FILESYSTEM.open(gifDir);
-      if (root)
+  while (1) // run forever
+  {
+
+    root = FILESYSTEM.open(gifDir);
+    if (root)
+    {
+      gifFile = root.openNextFile();
+      while (gifFile)
       {
-           gifFile = root.openNextFile();
-            while (gifFile)
-            {
-              if (!gifFile.isDirectory()) // play it
-              {
-                
-                // C-strings... urghh...                
-                memset(filePath, 0x0, sizeof(filePath));                
-                strcpy(filePath, gifFile.path());
-                
-                // Show it.
-                ShowGIF(filePath);
-               
-              }
-              gifFile.close();
-              gifFile = root.openNextFile();
-            }
-         root.close();
-      } // root
-      
-      delay(1000); // pause before restarting
-      
-   } // while
+        if (!gifFile.isDirectory()) // play it
+        {
+
+          // C-strings... urghh...
+          memset(filePath, 0x0, sizeof(filePath));
+          strcpy(filePath, gifFile.path());
+
+          // Show it.
+          ShowGIF(filePath);
+        }
+        gifFile.close();
+        gifFile = root.openNextFile();
+      }
+      root.close();
+    } // root
+
+    delay(1000); // pause before restarting
+
+  } // while
 }
